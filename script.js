e7989a1d49eba2d83929931e090daeb9ae19db66
Rustam Yuburov <rt138328@gmail.com>
2021-04-03 11:12:31 +0300

    Added math and operator functions

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
 
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case 'plus':
+            return add(num1, num2);
+            break;
+    
+        case 'minus':
+            return subtract(num1, num2);
+            break;
+        
+        case 'power':
+            return multiply(num1, num2);
+            break;
 
+        case 'division':
+            if (num2 === 0) return "Error! Can't divide by zero!";
+            return divide(num1, num2);
+            break;
+    }
+}
