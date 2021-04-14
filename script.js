c2840a667ec9c43d14cce6bf5adaeeba12f4558e
Rustam Yuburov <rt138328@gmail.com>
2021-04-14 13:01:20 +0300

    Finished project

--- script.js
+++ script.js
@@ -1,3 +1,89 @@
+// First getting all necessary DOM nodes
+const display = document.querySelector('.display');
+const clearButton = document.querySelector('.clear-btn');
+const deleteButton = document.querySelector('.delete-btn');
+const numbersButton = Array.from(document.querySelectorAll('.numbers'));
+const operatorsButton = Array.from(document.querySelectorAll('.operator'));
+const pointButton = document.querySelector('.point');
+const equalButton = document.querySelector('.equal');
+
+// Declare global veriables to use
+let currentOperator = null;
+let firstOperand = '';
+let secondOperand = '';
+let shouldResetScreen = false;
+
+// Setting EventListeners to buttons
+deleteButton.addEventListener('click', deleteNumber);
+equalButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+window.addEventListener('keydown', setInput);
+pointButton.addEventListener('click', addPoint)
+numbersButton.forEach(number => {
+    number.addEventListener('click',() => addToDisplay(number.textContent))
+});
+operatorsButton.forEach(operator => {
+    operator.addEventListener('click', () => setOperator(operator.textContent))
+});
+
+// EventListeners Functions
+function setInput(e) {
+    if (e.key >= 0 && e.key <= 9) addToDisplay(e.key);
+    if (e.key === '.') addPoint();
+    if (e.key === '=' || e.key === 'Enter') evaluate();
+    if (e.key === 'Backspace') deleteNumber();
+    if (e.key === 'Escape') clear();
+    if (e.key === '-' || e.key === '+' || e.key === '/' || e.key === '*') setOperator(e.key);
+}
+
+function addPoint() {
+    if (display.textContent.includes('.')) return;
+    display.textContent += '.';
+}
+
+function clear() {
+    display.textContent = '0';
+    currentOperator = null;
+    firstOperand = '';
+    secondOperand = '';
+}
+
+function addToDisplay(number) {
+    if (display.textContent.length === 9) return;
+    if (display.textContent === '0' || shouldResetScreen) resetDisplay();
+    display.textContent += number;
+}
+
+function deleteNumber() {
+    if (display.textContent.length === 1) return display.textContent = '0';
+    display.textContent = display.textContent.slice(0, -1);
+}
+
+function setOperator(oper) {
+    if (currentOperator !== null) evaluate();
+    firstOperand = display.textContent;
+    currentOperator = oper;
+    shouldResetScreen = true;
+}
+
+function evaluate(e) {
+    if (currentOperator === null || shouldResetScreen) return;
+    if (currentOperator === '/' && display.textContent === '0') {
+        alert('You can\'t divide by zero!');
+        clear();
+        return
+    }
+    secondOperand = display.textContent;
+    display.textContent = operate(currentOperator, firstOperand, secondOperand);
+    currentOperator = null;
+}
+
+// Helper Functions
+function resetDisplay() {
+    display.textContent = ''
+    shouldResetScreen = false;
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -12,22 +98,19 @@ function divide(a, b) {
 }
 
 function operate(operator, num1, num2) {
+    let x = +num1;
+    let y = +num2;
     switch (operator) {
-        case 'plus':
-            return add(num1, num2);
-            break;
+        case '+':
+            return add(x, y);
     
-        case 'minus':
-            return subtract(num1, num2);
-            break;
+        case '-':
+            return subtract(x, y);
         
-        case 'power':
-            return multiply(num1, num2);
-            break;
+        case '*':
+            return multiply(x, y);
 
-        case 'division':
-            if (num2 === 0) return "Error! Can't divide by zero!";
-            return divide(num1, num2);
-            break;
+        case '/':
+            return divide(x, y);
     }
 }
